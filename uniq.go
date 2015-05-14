e926d120d73834830358d977c2711254d901035f
donke <aka.kudoken@gmail.com>
2015-05-15 05:05:41 +0900

    Update uniq.go
    Add two functions.
    * ScanDuplicate
    * ScanUnique

--- uniq.go
+++ uniq.go
@@ -8,38 +8,117 @@ import (
 // UniqScanner ...
 type UniqScanner struct {
 	scanner *bufio.Scanner
-	prev    string
 	first   bool
+	last    bool
+	prev    string
+	this    string
+	text    string
+	repeats int
 }
 
 // NewScanner ...
 func NewScanner(r io.Reader) *UniqScanner {
-	scanner := &UniqScanner{scanner: bufio.NewScanner(r), first: true}
+	scanner := &UniqScanner{scanner: bufio.NewScanner(r), first: true, last: true}
 	return scanner
 }
 
 // Scan ...
 func (u *UniqScanner) Scan() bool {
 	if u.first {
+		u.first = false
 		if u.scanner.Scan() {
 			u.prev = u.scanner.Text()
-			u.first = false
+			u.text = u.prev
 			return true
-		}
-	} else {
-		for u.scanner.Scan() {
-			this := u.scanner.Text()
-			if u.prev != this {
-				u.prev = this
-				return true
-			}
 		}
 		return false
 	}
+
+	for u.scanner.Scan() {
+		u.this = u.scanner.Text()
+		if u.prev != u.this {
+			u.prev = u.this
+			u.text = u.prev
+			return true
+		}
+		u.repeats++
+	}
+
+	return false
+}
+
+// ScanDuplicate ...
+func (u *UniqScanner) ScanDuplicate() bool {
+	if u.first {
+		u.first = false
+		if u.scanner.Scan() {
+			u.prev = u.scanner.Text()
+		}
+	}
+
+	for u.scanner.Scan() {
+		u.this = u.scanner.Text()
+		if u.prev != u.this {
+			if u.repeats != 0 {
+				u.text = u.prev
+				u.prev = u.this
+				u.repeats = 0
+				return true
+			}
+			u.prev = u.this
+			u.repeats = 0
+		} else {
+			u.repeats++
+		}
+	}
+
+	if u.last {
+		u.last = false
+		if u.repeats != 0 {
+			u.text = u.prev
+			return true
+		}
+	}
+
+	return false
+}
+
+// ScanUnique ...
+func (u *UniqScanner) ScanUnique() bool {
+	if u.first {
+		u.first = false
+		if u.scanner.Scan() {
+			u.prev = u.scanner.Text()
+		}
+	}
+
+	for u.scanner.Scan() {
+		u.this = u.scanner.Text()
+		if u.prev != u.this {
+			if u.repeats == 0 {
+				u.text = u.prev
+				u.prev = u.this
+				return true
+			}
+			u.prev = u.this
+			u.repeats = 0
+		} else {
+			u.repeats++
+		}
+	}
+
+	if u.last {
+		u.last = false
+		if u.repeats == 0 {
+			u.text = u.prev
+			return true
+		}
+	}
+
 	return false
 }
 
 // Text ...
 func (u *UniqScanner) Text() string {
-	return u.prev
+	return u.text
 }
--- uniq_test.go
+++ uniq_test.go
@@ -6,30 +6,126 @@ import (
 )
 
 func TestNewScanner(t *testing.T) {
-	u := NewScanner(strings.NewReader("Spring has come."))
-	if u == nil {
+	s := NewScanner(strings.NewReader("Spring has come."))
+	if s == nil {
 		t.Fatal("Failed to create UniqScanner.")
 	}
 }
 
 func TestScan(t *testing.T) {
-	u := NewScanner(strings.NewReader("Lalala..."))
-	if !u.Scan() {
+	s := NewScanner(strings.NewReader("Lalala..."))
+	if !s.Scan() {
 		t.Error("Should advance the Scanner to the next token.")
 	}
-	if u.Scan() {
+	if s.Scan() {
 		t.Fatal("Should stop the scan.")
 	}
 }
 
-func TestText(t *testing.T) {
-	u := NewScanner(strings.NewReader("0\n0\n0\n0\n0\n1\n"))
-	u.Scan()
-	if u.Text() != "0" {
-		t.Errorf("Should scan while the same token. (%v)", u.Text())
+func TestScanDuplicate(t *testing.T) {
+	s := NewScanner(strings.NewReader("0\n0\n1\n"))
+	if !s.ScanDuplicate() {
+		t.Error("Should advance the Scanner to the next token.")
 	}
-	u.Scan()
-	if u.Text() != "1" {
-		t.Errorf("Should scan until the unique token. (%v)", u.Text())
+	if s.ScanDuplicate() {
+		t.Error("Should stop the scan.")
+	}
+}
+
+func TestScanUniq(t *testing.T) {
+	s := NewScanner(strings.NewReader("0\n1\n1\n"))
+	if !s.ScanUnique() {
+		t.Error("Should advance the Scanner to the next token.")
+	}
+	if s.ScanUnique() {
+		t.Error("Should stop the scan.")
+	}
+}
+
+func TestText(t *testing.T) {
+	s := NewScanner(strings.NewReader("0\n0\n1\n"))
+	s.Scan()
+	if s.Text() != "0" {
+		t.Errorf("Should scan while the same token. (%v)", s.Text())
+	}
+	s.Scan()
+	if s.Text() != "1" {
+		t.Errorf("Should scan until the unique token. (%v)", s.Text())
+	}
+}
+
+func TestDuplicateText(t *testing.T) {
+	var s *UniqScanner
+
+	s = NewScanner(strings.NewReader("0\n1\n1\n"))
+	s.ScanDuplicate()
+	if s.Text() != "1" {
+		t.Errorf("Should remove unique token. (%v)", s.Text())
+	}
+	if s.ScanDuplicate() {
+		t.Error("Should stop the scan.")
+	}
+
+	s = NewScanner(strings.NewReader("0\n0\n1\n"))
+	s.ScanDuplicate()
+	if s.Text() != "0" {
+		t.Errorf("Should remove unique token. (%v)", s.Text())
+	}
+	if s.ScanDuplicate() {
+		t.Error("Should stop the scan.")
+	}
+
+	s = NewScanner(strings.NewReader("0\n"))
+	if s.ScanDuplicate() {
+		t.Errorf("Should skip all tokens.")
+	}
+
+	s = NewScanner(strings.NewReader("0\n1\n"))
+	if s.ScanDuplicate() {
+		t.Errorf("Should skip all tokens.")
+	}
+}
+
+func TestUniqueText(t *testing.T) {
+	var s *UniqScanner
+
+	s = NewScanner(strings.NewReader("0\n0\n1\n"))
+	s.ScanUnique()
+	if s.Text() != "1" {
+		t.Errorf("Should remove duplica token. (%v)", s.Text())
+	}
+	if s.ScanUnique() {
+		t.Error("Should stop the scan.")
+	}
+
+	s = NewScanner(strings.NewReader("0\n1\n1\n"))
+	s.ScanUnique()
+	if s.Text() != "0" {
+		t.Errorf("Should remove duplica token. (%v)", s.Text())
+	}
+	if s.ScanUnique() {
+		t.Error("Should stop the scan.")
+	}
+
+	s = NewScanner(strings.NewReader("0\n"))
+	s.ScanUnique()
+	if s.Text() != "0" {
+		t.Error("Should advance the Scanner to the next token.")
+	}
+	if s.ScanUnique() {
+		t.Error("Should stop the scan.")
+	}
+
+	s = NewScanner(strings.NewReader("0\n1\n"))
+	s.ScanUnique()
+	if s.Text() != "0" {
+		t.Error("Should advance the Scanner to the next token.")
+	}
+	s.ScanUnique()
+	if s.Text() != "1" {
+		t.Error("Should advance the Scanner to the next token.")
+	}
+	if s.ScanUnique() {
+		t.Error("Should stop the scan.")
 	}
 }
